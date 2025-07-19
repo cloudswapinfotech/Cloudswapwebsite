e3b5c3bc57a4e48b9165979241d4088a4c142432
sv098058 <swapnil.vithalkar@oracle.com>
2025-07-19 19:36:04 +0530

    Adding files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-
-function submitForm(event) {
-    event.preventDefault();
-    alert("Thanks for contacting Cloudswap Infotech!");
-}
+// Make an API call using the base URL
+fetch(`${apiBaseURL}/api/greet`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Output API response
+        document.getElementById("greeting").innerText = data.message;
+    })
+    .catch(error => console.error("Error:", error));
